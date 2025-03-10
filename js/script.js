bed3ce99298650a933fdd136cfc41841518df62d
jacksonreismelo <jacksonreismelo@gmail.com>
2025-03-10 11:49:25 -0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    const errorMessage = document.getElementById('error-message');
+    errorMessage.innerText = ''; // Limpa a mensagem de erro
+
+    // O formulário é enviado via POST para login.php
+});
